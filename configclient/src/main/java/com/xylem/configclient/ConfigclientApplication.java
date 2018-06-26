45238f176aef78b7e8a5fdcde30bfd43de1d1538
P <Jayachandran.P@fluidtechnology.net>
2018-06-26 09:39:37 +0530

    JP-0001

--- configclient/src/main/java/com/xylem/configclient/ConfigclientApplication.java
+++ configclient/src/main/java/com/xylem/configclient/ConfigclientApplication.java
@@ -1,12 +1,33 @@
 package com.xylem.configclient;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+/**
+ * @RefreshScope should be there for all beans where we are required of
+ *               refreshed config server properties
+ */
+@RefreshScope
 public class ConfigclientApplication {
+
+	@Value("${user.role}")
+	private String role;
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConfigclientApplication.class, args);
+	}
+
+	@RequestMapping(value = "/whoami/{username}", method = RequestMethod.GET, produces = MediaType.TEXT_PLAIN_VALUE)
+	public String whoami(@PathVariable("username") String username) {
+		return String.format("Hello! You're %s and you'll become a(n) %s...\n", username, role);
 	}
 }
